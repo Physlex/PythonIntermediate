54dae37bb272374e7867a238f3c14a5cd813f66c
wila2720 <a.williamsletondre@uleth.ca>
2022-03-03 13:32:03 -0700

    Format finished

---
+++ formating/formating.py
@@ -0,0 +1,12 @@
+from math import pi
+
+name: str = 'Alexander Willet'
+age: int = 21
+
+print(f"\nSwapping Test:")
+a, b = 1, 2
+a, b = b, a
+print(a, b)
+print(f"\nFormating test:")
+print(f"Hello~ My name is {name} and I'm {age}")
+print(f"My favourite number is {pi}")
